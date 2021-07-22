e6c9645db1dad597b526bc9404b4628dd1433862
solrak <luiscarlosquesada@gmail.com>
2021-07-22 16:29:16 -0600

    Se clasifica incorectamente las instancias

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         SVM model = new SVM("Models/03_SVM_1500.model");
-        model.classify("Our_Deeds_are_the_Reason_of_this_earthquake_May_ALLAH_Forgive_us_all");
+        model.classify("ooked at my house it screams appropriation bc buddhas and stuff");
+        //model.classify("Our_Deeds_are_the_Reason_of_this_earthquake_May_ALLAH_Forgive_us_all");
     }
 } 
--- src/SVM.java
+++ src/SVM.java
@@ -1,96 +1,49 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
-import java.util.List;
 
-import weka.classifiers.functions.LibSVM;
-import weka.core.SerializationHelper;
-import weka.core.stemmers.NullStemmer;
-import weka.core.tokenizers.NGramTokenizer;
+import weka.classifiers.Classifier;
+import weka.classifiers.meta.FilteredClassifier;
+import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.Attribute;
-import weka.core.DenseInstance;
+import weka.core.SparseInstance;
+import weka.core.stemmers.NullStemmer;
+import weka.core.tokenizers.NGramTokenizer;
+import weka.filters.Filter;
+import weka.filters.MultiFilter;
 import weka.filters.unsupervised.attribute.NominalToString;
 import weka.filters.unsupervised.attribute.StringToWordVector;
-import weka.filters.Filter;
+
 
 public class SVM {
-    LibSVM svm = new LibSVM();
+    Classifier classifier;
+    Instances modelHeader;
+    FilteredClassifier fc;
 
-    public SVM(String path) {
-        try {
-            svm = (LibSVM) SerializationHelper.read(path);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public SVM(String path) throws IOException, ClassNotFoundException {
+        InputStream is = new BufferedInputStream(new FileInputStream(path));
+        ObjectInputStream objectStream = new ObjectInputStream(is);
+
+        classifier = (Classifier) objectStream.readObject();
+        modelHeader = (Instances) objectStream.readObject();
+
+        objectStream.close();
+
+        fc = new FilteredClassifier();
+        fc.setClassifier(classifier);
+        fc.setFilter(createMultiFilter());
+        
+        //try {
+        //    fc.buildClassifier(modelHeader);
+        //} catch (Exception e) {
+        //    e.printStackTrace();
+        //}
     }
-
-    public double classify(String instance) {
-        Instances dataset = createParametrizedDataset(instance);
-        try {
-            dataset = formatInstances(dataset);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return 0.0;
-    }
-
-    private Instances createParametrizedDataset(String inputText){
-        Attribute id = new Attribute("id");
-        
-        ArrayList<String> nominal = new ArrayList<String>(3);
-        nominal.add("fake_");
-        nominal.add("real_");
-        Attribute target = new Attribute("target", nominal);
-
-        ArrayList<String> nominaltext = new ArrayList<String>(1);
-        nominaltext.add(inputText);
-        Attribute text = new Attribute("text", nominaltext);
-        
-        ArrayList<Attribute> attr = new ArrayList<Attribute>();
-        attr.add(id);
-        attr.add(target);
-        attr.add(text);
-        
-        Instances dataset = new Instances("data", attr , 0);
-        dataset.setRelationName("data");
-
-        Instance inst = new DenseInstance(3);
-        inst.setValue(id, 1);
-        inst.setValue(target, "fake_");
-        inst.setValue(text, inputText);
-
-        dataset.add(inst);
-
-        return dataset;
-    }
-
-    private Instances formatInstances(Instances dataset) throws Exception {
-        Filter n2s = createParametrizedNominal2String();
-        Filter s2wv = createParametrizedString2WordVector();
-        
-
-        //Dios 
-        //Nos
-        //Ha
-        //Abandonado
-        
-        //
-        n2s.setInputFormat(dataset);
-        //
-        //Instances n2sApplied;
-        //n2sApplied = Filter.useFilter(dataset, n2s);
-        //
-        //s2wv.setInputFormat(n2sApplied);
-        //
-        //Instances s2wvApplied;
-        //s2wvApplied = Filter.useFilter(n2sApplied, s2wv);
-
-        //return s2wvApplied;
-
-        return dataset;
-    }
-
+    
     private Filter createParametrizedNominal2String(){
         String[] options = {"-C", "3"};
         Filter n2s = new NominalToString();
@@ -104,7 +57,7 @@ public class SVM {
     }
 
     private Filter createParametrizedString2WordVector(){
-        String tokenDelimiters = "";
+        String tokenDelimiters = "\\r\\t.,;:\\\'\\\"()?!_";
         String[] tokenOptions = {"-max", "3", "-min", "1", "-delimiters", tokenDelimiters};
         String[] s2wvOptions = {"-R", "3", "-W", "1000", "-prune-rate", "1.0", "-L"};
         
@@ -127,5 +80,73 @@ public class SVM {
         }
 
         return s2wv;
+    }
+
+    private MultiFilter createMultiFilter(){
+        MultiFilter filter = new MultiFilter();
+        Filter filters[] = new Filter[2];
+        
+        filters[0] = createParametrizedNominal2String();
+        filters[1] = createParametrizedString2WordVector();
+
+        filter.setFilters(filters);
+        return filter;
+    }
+
+    private Instance createInstance(String inputText){
+        //  Set ID
+        Attribute id = new Attribute("id");
+        
+        //  Set classes
+        ArrayList<String> nominal = new ArrayList<String>(3);
+        nominal.add("fake");
+        nominal.add("real");
+        Attribute target = new Attribute("target", nominal);
+
+        //  Set nominal text
+        ArrayList<String> nominaltext = new ArrayList<String>(1);
+        nominaltext.add(inputText);
+        Attribute text = new Attribute("text", nominaltext);
+        
+        //Set attributes
+        ArrayList<Attribute> attr = new ArrayList<Attribute>();
+        attr.add(id);
+        attr.add(target);
+        attr.add(text);
+        Instances dataset = new Instances("data", attr, 0);
+        dataset.setClassIndex(1);
+        
+        //    Set Instance
+        Instance inst = new SparseInstance(3);
+        inst.setValue(id, 1);
+        inst.setValue(target, "fake");
+        inst.setValue(text, inputText);
+
+        dataset.add(inst);
+
+        try {
+            fc.buildClassifier(dataset);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        inst.setDataset(dataset);
+
+        try {
+            double value = fc.classifyInstance(inst);
+            System.out.println(dataset.classAttribute().value((int)value));
+       } catch (Exception e) {
+            e.printStackTrace();
+       }
+
+        return inst;
+    }
+
+    public void classify(String text){
+        Instance inst = createInstance(text);
+
+        // System.out.println(inst);
+       
+       
     }
 }
