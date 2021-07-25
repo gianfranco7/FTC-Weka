509e80a5c9a3fa48f881e0dfa9252ea7dd5de5bf
solrak <luiscarlosquesada@gmail.com>
2021-07-25 01:55:56 -0600

    Clasificaciones correctas, GUI terminado, puede que hayan algunos bugs pero es funcional

--- src/App.java
+++ src/App.java
@@ -1,18 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        GUI gui = new GUI(700, 500);
-       
-        //Dataset trainer = new Dataset();
- //
-        //System.out.println("_____________________________________________________\n");
-        //System.out.println("                  Cargando dataset...");
-        //trainer.load("Models/train.arff");
-        //trainer.applyFilters();
- //
-        //SVMClassifier model = new SVMClassifier(trainer, "Models/SVMTrainedModel.model");
+        GUI gui = new GUI(700, 600);
 
+        Dataset trainer = new Dataset();
+        System.out.println("_____________________________________________________\n");
+        System.out.println("                  Cargando dataset...");
+        trainer.load("Models/train.arff");
+        trainer.applyFilters();
+
+        SVMClassifier model = new SVMClassifier(trainer, "Models/SVMTrainedModel.model");
+        gui.setClassifier(model);
         gui.setPredictionState();
-        //model.classify("There's an emergency evacuation happening now in the building across the street");
-
     }
-} 
+}
--- src/GUI.java
+++ src/GUI.java
@@ -13,6 +13,8 @@ int x,y;
     JButton resetButton;
     JButton classifyButton;
 
+    JLabel result;
+
     SVMClassifier classifier;
 
     public GUI(int x, int y){
@@ -24,7 +26,8 @@ int x,y;
         frame.getContentPane().setBackground(new Color(7,9,15));
 
         frame.setSize(x, y);
-        label = new JLabel("Cargando espere...", SwingConstants.CENTER);
+        label = new JLabel("Cargando espere...");
+        label.setBounds(150, 150, 800, 100);
         label.setFont(new Font(label.getFont().getName(), Font.PLAIN, 50));
         label.setForeground(new Color(255,255,255));
         frame.add(label);
@@ -42,7 +45,13 @@ int x,y;
         resetButton.setBounds(500,350,100,40);
         resetButton.setForeground(new Color(255,255,255)); 
         
+        classifyButton.addActionListener(e -> classify());
         resetButton.addActionListener(e -> clearToPredict());
+
+        result = new JLabel("Resultado");
+        result.setFont(new Font(label.getFont().getName(), Font.PLAIN, 50));
+        result.setBounds(225,400,300,100);
+        result.setForeground(new Color(125,125,125));
     }
 
     //Ya se que esto es feo y deberia estar afuera pero no tengo tiempo ok?
@@ -52,8 +61,7 @@ int x,y;
 
     public void setPredictionState(){
         label.setText("Texto a evaluar");
-        label.setBounds((x / 2) - (700/2), (int) (y / 10), 700, 40);
-        
+        label.setBounds(175, (int) (y / 10), 700, 40);
         textArea = new JTextArea(10, 512);
         textArea.setLineWrap(true);
         textArea.setFont(new Font(label.getFont().getName(), Font.PLAIN, 15));
@@ -65,11 +73,34 @@ int x,y;
         frame.add(scrollPane);
         frame.add(resetButton);
         frame.add(classifyButton);
+        frame.add(result);
 
         frame.setVisible(false);
         frame.setVisible(true);
     }
-    public void clearToPredict(){
+    private void clearToPredict(){
         textArea.setText("");
+        result.setForeground(new Color(125,125,125));
+        result.setText("Resultado");
+    }
+
+    private void classify(){
+        String text = textArea.getText();
+        boolean res;
+        try {
+            res = classifier.classify(text);
+            System.out.println("ok");
+
+            if(res){
+                result.setForeground(new Color(142,166,4));
+                result.setText("Verdadero");
+            }
+            else{
+                result.setForeground(new Color(191,49,0));
+                result.setText("Falso");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
